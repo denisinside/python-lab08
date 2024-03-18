e993af7a51f24a080fae0265f2ac38adc20cff15
denisinside <d.shvachka@ukma.edu.ua>
2024-03-18 23:34:36 +0200

    Methods are done. Testes for them are made. Main was constucted. I am died.

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from app.io import input
+from app.io import output
+from tests import test_input
 
 
 def main():
-    pass
+    text = input.input_from_console()
+
+    output.output_to_console("From console: " + text)
+
+    output.output_to_file(text, 'data//test_python.txt')
+
+    text_from_file_builtin = input.input_from_file('data//test_python.txt')
+
+    output.output_to_console("From file builtin: " + text_from_file_builtin)
+
+    output.output_to_file_pandas(text_from_file_builtin, 'data//test_pandos.csv')
+
+    text_from_file_pandas = input.input_from_file_pandas('data//test_pandos.csv')
+
+    output.output_to_console("From file pandas: " + text_from_file_pandas)
 
 
 if __name__ == "__main__":
